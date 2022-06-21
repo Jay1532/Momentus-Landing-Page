241539ba44d800286451c62c5fb3a17c471d7e5f
Jay Bhalala <bhalalajay722@gmail.com>
2022-06-20 19:15:17 -0500

    ui code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,348 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg viewBox="0 0 1440 1890">
+    <defs>
+      <style>
+        {
+          ".a{clip-path:url(#i);}.b,.g,.v{fill:#fff;}.c{fill:url(#a);}.d{fill:#282828;font-size:26px;letter-spacing:-0.038em;}.d,.s,.x{font-family:SegoeUI-Bold, Segoe UI;font-weight:700;}.e{fill:#8593a3;}.e,.l,.u,.v,.w{font-size:16px;}.e,.g,.h,.i,.j,.l,.u,.v,.w{font-family:SegoeUI, Segoe UI;}.f{fill:#ed1865;}.g{font-size:18px;}.h{font-size:60px;letter-spacing:-0.017em;}.i{font-size:40px;letter-spacing:-0.025em;}.j,.w{fill:#757095;}.j{font-size:20px;}.k{fill:#c4c4c4;opacity:0.3;}.l,.s,.u{fill:#181433;}.m{fill:#f8f8fe;}.n{fill:url(#d);}.o{fill:url(#e);}.p{font-size:15px;}.p,.q{font-family:Lato-Regular, Lato;}.q{font-size:50px;text-decoration:underline;}.r{fill:url(#f);}.s,.x{font-size:21px;}.t{fill:#f9f9f9;}.u{opacity:0.2;}.x{fill:#1b1c31;}.y{filter:url(#b);}"
+        }
+      </style>
+      <linearGradient
+        id="a"
+        x1={0.511}
+        y1={0.007}
+        x2={0.679}
+        y2={0.677}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopColor="#ed1865" stopOpacity={0.98} />
+        <stop offset={1} stopColor="#fff" stopOpacity={0} />
+      </linearGradient>
+      <filter
+        id="b"
+        x={11}
+        y={350}
+        width={489}
+        height={355}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy={38} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={50} result="c" />
+        <feFlood floodColor="#05796b" floodOpacity={0.2} />
+        <feComposite operator="in" in2="c" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <linearGradient
+        id="d"
+        x1={0.511}
+        y1={0.007}
+        x2={0.679}
+        y2={0.677}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopColor="#fc6262" stopOpacity={0.98} />
+        <stop offset={1} stopColor="#fff" stopOpacity={0} />
+      </linearGradient>
+      <linearGradient
+        id="e"
+        x1={0.511}
+        y1={0.007}
+        x2={0.679}
+        y2={0.677}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopOpacity={0.98} />
+        <stop offset={1} stopColor="#fff" stopOpacity={0} />
+      </linearGradient>
+      <linearGradient
+        id="f"
+        x1={0.511}
+        y1={0.993}
+        x2={0.679}
+        y2={0.323}
+        xlinkHref="#e"
+      />
+      <clipPath id="i">
+        <rect width={1440} height={1890} />
+      </clipPath>
+    </defs>
+    <g id="h" className="a">
+      <rect className="b" width={1440} height={1890} />
+      <g transform="translate(-572.699 -287.312)">
+        <path
+          className="b"
+          d="M0,0H1440V1024H0Z"
+          transform="translate(572.699 315.312)"
+        />
+        <circle
+          className="c"
+          cx={410}
+          cy={410}
+          r={410}
+          transform="matrix(0.966, 0.259, -0.259, 0.966, 1503.931, 0)"
+        />
+        <g transform="translate(698.699 849.312)">
+          <g transform="translate(634 -45)">
+            <text className="d">
+              <tspan x={0} y={28}>
+                {"About Us"}
+              </tspan>
+            </text>
+            <text className="e" transform="translate(0 52)">
+              <tspan x={0} y={17}>
+                {
+                  "Momentus plans to offer infrastructure services to support the growing "
+                }
+              </tspan>
+              <tspan x={0} y={42}>
+                {
+                  "space economy. With in-space transportation as our core service, our "
+                }
+              </tspan>
+              <tspan x={0} y={67}>
+                {
+                  "goal is to help our customers optimize the destination of space."
+                }
+              </tspan>
+            </text>
+          </g>
+        </g>
+        <g transform="translate(722.699 542.312)">
+          <g transform="translate(11 207)">
+            <g className="y" transform="matrix(1, 0, 0, 1, -161, -462)">
+              <rect
+                className="f"
+                width={189}
+                height={55}
+                rx={27.5}
+                transform="translate(161 462)"
+              />
+            </g>
+            <text className="g" transform="translate(30 15)">
+              <tspan x={19.1} y={19}>
+                {"Learn More"}
+              </tspan>
+            </text>
+          </g>
+          <text className="h" transform="translate(11)">
+            <tspan x={0} y={65}>
+              {"The Space Infrastructure "}
+            </tspan>
+            <tspan x={0} y={145}>
+              {"Company"}
+            </tspan>
+          </text>
+        </g>
+        <g transform="translate(1042.199 1168.812)">
+          <text className="i">
+            <tspan x={154.514} y={43}>
+              {"Our Service"}
+            </tspan>
+          </text>
+          <text className="j" transform="translate(0.5 71)">
+            <tspan x={6.055} y={22}>
+              {"Momentus intends to use a multi-pronged approach to "}
+            </tspan>
+            <tspan x={6.265} y={57}>
+              {"become a provider of three critical functions in the new "}
+            </tspan>
+            <tspan x={24.185} y={92}>
+              {"space economy: Space Transportation, Satellite as a "}
+            </tspan>
+            <tspan x={114.888} y={127}>
+              {"Service, and In-Orbit Servicing."}
+            </tspan>
+          </text>
+        </g>
+        <rect
+          className="m"
+          width={1169}
+          height={2}
+          transform="translate(708.699 1094.312)"
+        />
+        <circle
+          className="n"
+          cx={410}
+          cy={410}
+          r={410}
+          transform="translate(960.398 1430.776) rotate(101)"
+        />
+        <circle
+          className="o"
+          cx={410}
+          cy={410}
+          r={410}
+          transform="matrix(0.966, 0.259, -0.259, 0.966, 1854.785, 929.168)"
+        />
+        <g transform="translate(927.699 316.312)">
+          <text className="p" transform="translate(0 13)">
+            <tspan x={60.536} y={15}>
+              {"Press"}
+            </tspan>
+          </text>
+          <text className="p" transform="translate(133 13)">
+            <tspan x={48} y={15}>
+              {"Investors"}
+            </tspan>
+          </text>
+          <text className="p" transform="translate(289 13)">
+            <tspan x={50.873} y={15}>
+              {"Services"}
+            </tspan>
+          </text>
+          <text className="p" transform="translate(445 13)">
+            <tspan x={52.811} y={15}>
+              {"Careers"}
+            </tspan>
+          </text>
+          <text className="p" transform="translate(594 13)">
+            <tspan x={50.636} y={15}>
+              {"Updates"}
+            </tspan>
+          </text>
+          <text className="p" transform="translate(750 13)">
+            <tspan x={7.924} y={15}>
+              {"Vigoride\u2122 First Flight"}
+            </tspan>
+          </text>
+          <text className="p" transform="translate(906 13)">
+            <tspan x={51.994} y={15}>
+              {"Contact"}
+            </tspan>
+          </text>
+          <text className="q" transform="translate(-313)">
+            <tspan x={32.738} y={49}>
+              {"Momentus"}
+            </tspan>
+          </text>
+        </g>
+        <rect
+          className="m"
+          width={1168}
+          height={2}
+          transform="translate(708.699 1459.312)"
+        />
+        <ellipse
+          className="r"
+          cx={410}
+          cy={333}
+          rx={410}
+          ry={333}
+          transform="matrix(0.966, 0.259, -0.259, 0.966, 203.856, 386.312)"
+        />
+        <circle
+          className="c"
+          cx={410}
+          cy={410}
+          r={410}
+          transform="matrix(0.966, 0.259, -0.259, 0.966, 1631.645, 1904.406)"
+        />
+      </g>
+      <g transform="translate(-174 -5712.75)">
+        <g transform="translate(76 185)">
+          <text className="s" transform="translate(923 6978)">
+            <tspan x={0} y={23}>
+              {"Sign Up To Receive Updates"}
+            </tspan>
+          </text>
+          <rect
+            className="t"
+            width={389}
+            height={52}
+            transform="translate(923 7026)"
+          />
+          <text className="u" transform="translate(938 7041)">
+            <tspan x={0} y={17}>
+              {"Your email address"}
+            </tspan>
+          </text>
+          <rect
+            className="f"
+            width={149}
+            height={52}
+            transform="translate(1163 7026)"
+          />
+          <text className="v" transform="translate(1213 7041)">
+            <tspan x={0} y={17}>
+              {"Submit"}
+            </tspan>
+          </text>
+        </g>
+        <g transform="translate(346 -0.75)">
+          <text className="w" transform="translate(54 7115.5)">
+            <tspan x={0} y={17}>
+              {"See Latest Updates"}
+            </tspan>
+          </text>
+          <g transform="translate(532 169.5)">
+            <text className="x" transform="translate(-478 6801.5)">
+              <tspan x={0} y={23}>
+                {"If you're interested in flying with Momentus, "}
+              </tspan>
+              <tspan x={0} y={51}>
+                {"please contact us sales@momentus.space"}
+              </tspan>
+              <tspan x={0} y={79} />
+              <tspan x={0} y={107} />
+              <tspan x={0} y={135}>
+                {"Blog"}
+              </tspan>
+              <tspan x={0} y={163} />
+              <tspan x={0} y={191}>
+                {"Investors"}
+              </tspan>
+              <tspan x={0} y={219} />
+              <tspan x={0} y={247} />
+              <tspan x={0} y={275}>
+                {"Partnerships"}
+              </tspan>
+              <tspan x={0} y={303} />
+              <tspan x={0} y={331}>
+                {"IRL"}
+              </tspan>
+              <tspan x={0} y={359} />
+              <tspan x={0} y={387} />
+              <tspan x={0} y={415} />
+              <tspan x={0} y={443}>
+                {"Jobs"}
+              </tspan>
+            </text>
+          </g>
+          <text className="w" transform="translate(54 7171.5)">
+            <tspan x={0} y={17}>
+              {"Investor Information"}
+            </tspan>
+            <tspan x={0} y={45}>
+              {"investors@momentus.space"}
+            </tspan>
+          </text>
+          <text className="w" transform="translate(54 7256.5)">
+            <tspan x={0} y={17}>
+              {"partnerships@momentus.space"}
+            </tspan>
+          </text>
+          <text className="w" transform="translate(54 7312.5)">
+            <tspan x={0} y={17} xmlSpace="preserve">
+              {"Momentus Inc.       "}
+            </tspan>
+            <tspan x={0} y={45}>
+              {"3901 N. First Street, San Jose, CA 95134"}
+            </tspan>
+            <tspan x={0} y={73}>
+              {"+1 (650) 564-7820"}
+            </tspan>
+          </text>
+          <text className="w" transform="translate(54 7424.5)">
+            <tspan x={0} y={17}>
+              {"jobs@momentus.space"}
+            </tspan>
+          </text>
+        </g>
+      </g>
+    </g>
+  </svg>
   );
 }
 
